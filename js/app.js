b41c5f6b953cade9e393b9fd6d9cb7f9a2e93f17
Shelby Harner <shelby.harner@gmail.com>
2021-01-16 19:09:55 -0800

    add JS for correct answer and alert response, incorrect response loop and exhausted number of chances alert response

--- js/app.js
+++ js/app.js
@@ -1,86 +1,102 @@
 'use strict';
 
-// console.log('Greetings Earthlings!');
+// // console.log('Greetings Earthlings!');
 
-// ask for user name and return response with greeting and their name
+// // ask for user name and return response with greeting and their name
 
-var userName = prompt('Greetings! What is your name?');
-alert('Welcome to my page ' + userName + '!');
+// var userName = prompt('Greetings! What is your name?');
+// alert('Welcome to my page ' + userName + '!');
 
-// prompt user with five yes or no questions
-// answers can be yes, no, y, n - IN ANY CASE
-// alert response back to user with correct or incorrect message
+// // prompt user with five yes or no questions
+// // answers can be yes, no, y, n - IN ANY CASE
+// // alert response back to user with correct or incorrect message
 
-// question one
-var homeLocation = prompt('Do I live in Queen Anne?').toLowerCase();
+// // question one
+// var homeLocation = prompt('Do I live in Queen Anne?').toLowerCase();
 
-if(homeLocation === 'yes' || homeLocation === 'y') {
-  // console.log('You are correct!');
-  alert('You are correct!');
-} else {
-  alert('Sorry, that is wrong.');
-}
+// if(homeLocation === 'yes' || homeLocation === 'y') {
+//   // console.log('You are correct!');
+//   alert('You are correct!');
+// } else {
+//   alert('Sorry, that is wrong.');
+// }
 
-// question two
-var tattoo = prompt('Do I have a tattoo?').toLowerCase();
+// // question two
+// var tattoo = prompt('Do I have a tattoo?').toLowerCase();
 
-if(tattoo === 'no' || tattoo === 'n') {
-  // console.log('Yes, that is correct!');
-  alert('Yes, that is correct!');
-} else {
-  alert('Nope, wrong.');
-}
+// if(tattoo === 'no' || tattoo === 'n') {
+//   // console.log('Yes, that is correct!');
+//   alert('Yes, that is correct!');
+// } else {
+//   alert('Nope, wrong.');
+// }
 
-// question three
-var siblings = prompt('Do I have siblings?').toLowerCase();
+// // question three
+// var siblings = prompt('Do I have siblings?').toLowerCase();
 
-if(siblings === 'yes' || siblings === 'y') {
-  // console.log('Yes!');
-  alert('Yes!');
-} else {
-  alert('Oops! No.');
-}
+// if(siblings === 'yes' || siblings === 'y') {
+//   // console.log('Yes!');
+//   alert('Yes!');
+// } else {
+//   alert('Oops! No.');
+// }
 
-// question four
-var petType = prompt('Do I have a dog?').toLowerCase();
+// // question four
+// var petType = prompt('Do I have a dog?').toLowerCase();
 
-if(petType === 'no' || petType === 'n') {
-  // console.log('Great job!');
-  alert('Great job!');
-} else {
-  alert('Oh, no.');
-}
+// if(petType === 'no' || petType === 'n') {
+//   // console.log('Great job!');
+//   alert('Great job!');
+// } else {
+//   alert('Oh, no.');
+// }
 
-// question five
-var crafts = prompt('Do I love crafting?').toLowerCase();
+// // question five
+// var crafts = prompt('Do I love crafting?').toLowerCase();
 
-if(crafts === 'yes' || crafts === 'y') {
-  // console.log('Yes, ' + userName + '! Good answer!');
-  alert('Yes, ' + userName + '! Good answer!');
-} else {
-  alert('Sorry, ' + userName + '. Incorrect.');
-}
+// if(crafts === 'yes' || crafts === 'y') {
+//   // console.log('Yes, ' + userName + '! Good answer!');
+//   alert('Yes, ' + userName + '! Good answer!');
+// } else {
+//   alert('Sorry, ' + userName + '. Incorrect.');
+// }
 
-var plantGuesses = 4;
-var myPlants = 9; 
+// var plantGuesses = 4;
+// var myPlants = 9; 
 
-var plantQuestion = parseInt(prompt('How many houseplants do I have?'));
+// var plantQuestion = parseInt(prompt('How many houseplants do I have?'));
 
-for (var i = 1; i < plantGuesses; i++){
+// for (var i = 1; i < plantGuesses; i++){
 
-  if (plantQuestion === myPlants){
-    alert(`${myPlants} is the correct number!`);
-    break;
-  } else if(plantQuestion < myPlants){
-    plantQuestion = parseInt(prompt('Sorry, too low.'));
-  } else if(plantQuestion > myPlants){
-    plantQuestion = parseInt(prompt('Oops, too high.'));
-  }
-}
-if(i === plantGuesses){
-    alert(`Wrong! ${myPlants} was the right answer.`);
-  }
+//   if (plantQuestion === myPlants){
+//     alert(`${myPlants} is the correct number!`);
+//     break;
+//   } else if(plantQuestion < myPlants){
+//     plantQuestion = parseInt(prompt('Sorry, too low.'));
+//   } else if(plantQuestion > myPlants){
+//     plantQuestion = parseInt(prompt('Oops, too high.'));
+//   }
+// }
+// if(i === plantGuesses){
+//     alert(`Wrong! ${myPlants} was the right answer.`);
+//   }
 
-var favoriteFoods = ['Sushi', 'Mac and Cheese', 'Pho', 'Pizza'];
+var foodGuesses = 6;
+var favoriteFoods = ['sushi', 'mac and cheese', 'pho', 'pizza'];
   // console.log(favoriteFoods);
-var foodQuestion = prompt('What is a favorite food of mine?');
+var foodQuestion = prompt('What is a favorite food of mine?').toLowerCase();
+  // console.log(foodQuestion);
+
+for (var i = 0; i < foodGuesses; i++){
+
+  if (favoriteFoods[i] === foodQuestion){
+    alert(`Yes, ${foodQuestion} is delicious!`);
+    break;
+  } else if (favoriteFoods[i] !== foodQuestion){
+    prompt('Try again.');
+  }
+}
+
+if(i === foodGuesses){
+  alert(`No, ${favoriteFoods} were the right options.`);
+  }
